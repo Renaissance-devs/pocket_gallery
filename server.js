8927b02a32320c97071e3844de76377a51b9c1f1
David Vloedman <djvloedman@gmail.com>
2019-11-20 08:23:23 -0800

    left over stuff to merge

--- server.js
+++ server.js
@@ -85,7 +85,6 @@ function getArt(request, response) {
 
 function getOneWork(request, response) {
   getGalleries().then(galleries => {
-    // console.log(galleries);
     let SQL = `SELECT * FROM works WHERE id=$1`;
     const values = [request.params.id];
     return client.query(SQL, values).then(results => {
@@ -124,7 +123,10 @@ function searchResults(request, response) {
         response.render('searches/noResults');
       } else {
         let works = apiResponse.body.records.filter(work => work.images.length >= 1).map(artResult => new Art(artResult));
-        getGalleries().then(galleries => response.render('searches/show', {works: works, galleries: galleries.rows}));
+        getGalleries().then(galleries => response.render('searches/show', {
+          works: works,
+          galleries: galleries.rows
+        }));
       }
     })
     .catch((error, response) => handleError(error, response));
