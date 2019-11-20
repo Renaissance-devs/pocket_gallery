8a524b2c4b2fcc148dbe630b2668b6aeb22afb44
David Vloedman <djvloedman@gmail.com>
2019-11-20 13:35:28 -0800

    Pull out gallery to new table, add create gallery, modify all queries to reflect table schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,10 @@
 DROP TABLE IF EXISTS works;
+DROP TABLE IF EXISTS gallery;
+
+CREATE TABLE gallery (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)  
+);
 
 CREATE TABLE works (
   id SERIAL PRIMARY KEY,
@@ -6,26 +12,39 @@ CREATE TABLE works (
   title VARCHAR(255),
   image_url VARCHAR(255),
   century VARCHAR(255),
-  gallery VARCHAR(255)
+  gallery_id INT,
+  CONSTRAINT fk_gallery FOREIGN KEY (gallery_id)  REFERENCES gallery(id)
 );
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Eugene Speicher', 'Landscape', 'https://nrs.harvard.edu/urn-3:HUAM:LEG259441', '19th-20th century', 'landscapes');
+-- ALTER TABLE works ADD 
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Joseph Lindon Smith', 'Mountain Landscape', 'https://nrs.harvard.edu/urn-3:HUAM:INV006558_dynmc', '19th-20th century', 'mountains');
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Joshua Johnson', 'Portrait of a Girl', 'https://nrs.harvard.edu/urn-3:HUAM:DDC104098_dynmc', '19th century', 'portraits');
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Jakob Ochtervelt', 'Portrait of a Family', 'https://nrs.harvard.edu/urn-3:HUAM:30011_dynmc', '17th century', 'life');
+INSERT INTO gallery (name) VALUES ('landscapes');
+INSERT INTO gallery (name) VALUES ('portraits');
+INSERT INTO gallery (name) VALUES ('mountain');
+INSERT INTO gallery (name) VALUES ('life');
+INSERT INTO gallery (name) VALUES ('skull');
+INSERT INTO gallery (name) VALUES ('religious');
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Karl von Blaas', 'The Miraculous Translation of the Body of Saint Catherine of Alexandria to Sinai', 'https://nrs.harvard.edu/urn-3:HUAM:46448_dynmc', '19th century', 'religious');
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Mary Cassatt', 'Woman on a Striped Sofa with a Dog', 'https://nrs.harvard.edu/urn-3:HUAM:DDC252719_dynmc', '19th century', 'life');
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Eugene Speicher', 'Landscape', 'https://nrs.harvard.edu/urn-3:HUAM:LEG259441', '19th-20th century', 1);
 
-INSERT INTO works (artist, title, image_url, century, gallery)
-VALUES ('Alexander Pope', 'Skull', 'https://nrs.harvard.edu/urn-3:HUAM:770200', '19th-20th century century', 'skull');
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Joseph Lindon Smith', 'Mountain Landscape', 'https://nrs.harvard.edu/urn-3:HUAM:INV006558_dynmc', '19th-20th century', 3);
+
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Joshua Johnson', 'Portrait of a Girl', 'https://nrs.harvard.edu/urn-3:HUAM:DDC104098_dynmc', '19th century', 2);
+
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Jakob Ochtervelt', 'Portrait of a Family', 'https://nrs.harvard.edu/urn-3:HUAM:30011_dynmc', '17th century', 4);
+
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Karl von Blaas', 'The Miraculous Translation of the Body of Saint Catherine of Alexandria to Sinai', 'https://nrs.harvard.edu/urn-3:HUAM:46448_dynmc', '19th century', 6);
+
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Mary Cassatt', 'Woman on a Striped Sofa with a Dog', 'https://nrs.harvard.edu/urn-3:HUAM:DDC252719_dynmc', '19th century', 4);
+
+INSERT INTO works (artist, title, image_url, century, gallery_id)
+VALUES ('Alexander Pope', 'Skull', 'https://nrs.harvard.edu/urn-3:HUAM:770200', '19th-20th century century', 5);
--- server.js
+++ server.js
@@ -29,13 +29,18 @@ app.set('view engine', 'ejs');
 //
 //********************************************************************* */ 
 
-function Art(info) {
+function Art(info, gallery) {
   const placeholderImage = './public/assets/placeholder.jpg';
 
   this.artist = info.peoplecount > 0 ? info.people[0].name : 'No artist available';
   this.title = info.title || 'No title available';
   this.image_url = info.images[0] ? info.images[0].baseimageurl : placeholderImage;
   this.century = info.century || 'We don\'t have this information';
+  this.gallery = gallery;
+}
+
+function Gallery(gallery) {
+  this.name = gallery.name;
 }
 
 
@@ -56,11 +61,13 @@ app.use(methodOverride((request, response) => {
 app.get('/', getArt);
 app.get('/searches', search);
 app.post('/gallery', gallerySelect);
+app.put('/gallery', createGallery);
 app.post('/searches/results', searchResults);
 app.post('/works', createWork);
 app.get('/works/:id', getOneWork);
 app.put('/works/:id', updateWork);
 app.delete('/works/:id', deleteWork);
+
 app.get('*', (request, response) => response.render('pages/error', {
   error: '404 Page Not Found'
 }));
@@ -75,12 +82,13 @@ function gallerySelect(request, response) {
   if (request.body.gallery === 'all') {
     response.redirect('/');
   }
-  let SQL = `SELECT * FROM works WHERE gallery=$1;`;
+  console.log();
+
+  let SQL = `SELECT * FROM works JOIN gallery ON works.gallery_id=gallery.id WHERE name=$1`;
   const values = [request.body.gallery];
-  console.log(request.body.gallery);
+
   return client.query(SQL, values)
     .then(results => {
-      console.log(results.rows);
       getGalleries().then(galleries => response.render('pages/index', {
         result: results.rows,
         count: results.rows.length,
@@ -90,7 +98,7 @@ function gallerySelect(request, response) {
 }
 
 function getArt(request, response) {
-  let SQL = `SELECT * FROM works;`;
+  let SQL = `SELECT * FROM works JOIN gallery ON works.gallery_id=gallery.id`;
   return client.query(SQL)
     .then(results => {
       getGalleries().then(galleries => response.render('pages/index', {
@@ -104,7 +112,7 @@ function getArt(request, response) {
 
 function getOneWork(request, response) {
   getGalleries().then(galleries => {
-    let SQL = `SELECT * FROM works WHERE id=$1`;
+    let SQL = `SELECT * FROM works JOIN gallery ON works.gallery_id=gallery.id WHERE works.id=$1`;
     const values = [request.params.id];
     return client.query(SQL, values).then(results => {
       response.render('works/detail', {
@@ -135,6 +143,7 @@ function searchResults(request, response) {
     param = 'q=color';
     search = request.body.search[0];
   }
+
   let url = `https://api.harvardartmuseums.org/object?${param}=${search}&classification=Paintings&apikey=${process.env.ART_API_KEY}`;
   superagent.get(url)
     .then(apiResponse => {
@@ -142,31 +151,43 @@ function searchResults(request, response) {
         response.render('searches/noResults');
       } else {
         let works = apiResponse.body.records.filter(work => work.images.length >= 1).map(artResult => new Art(artResult));
-        getGalleries().then(galleries => response.render('searches/show', {
-          works: works,
-          galleries: galleries.rows
-        }));
+        getGalleries().then(galleries => {
+          response.render('searches/show', {
+            works: works,
+            galleries: galleries.rows
+          });
+        })
       }
-    })
-    .catch((error, response) => handleError(error, response));
+    });
 }
+
+
+
+
 
 
 function updateWork(request, response) {
   const gallery = request.body.gallery;
   const id = request.params.id;
-  const values = [gallery, id];
-  let SQL = `UPDATE works SET gallery=$1 WHERE id=$2 RETURNING *`;
 
-  return client.query(SQL, values).then(results => {
-    response.redirect(`/works/${id}`);
-  }).catch((error, response) => handleError(error, response));
+
+
+  const galleryQuery = `SELECT id FROM gallery WHERE name=$1`;
+  const galleryValues = [gallery];
+
+  client.query(galleryQuery, galleryValues).then(galleryId => {
+    const values = [galleryId.rows[0].id, id];
+    let SQL = `UPDATE works SET gallery_id=$1 WHERE id=$2 RETURNING *`;
+    return client.query(SQL, values)
+      .then(() => response.redirect(`/works/${id}`))
+  }).catch(error => console.error(error.error));
+
 }
 
 function deleteWork(request, response) {
   const values = [request.params.id];
   const SQL = `DELETE FROM works WHERE id=$1`;
-  client.query(SQL, values).then(_ => response.redirect('/')).catch((error, response) => handleError(error, response));
+  client.query(SQL, values).then(() => response.redirect('/')).catch((error, response) => handleError(error, response));
 }
 
 function createWork(request, response) {
@@ -174,19 +195,27 @@ function createWork(request, response) {
     artist,
     title,
     image_url,
-    gallery,
     century
   } = request.body;
-  console.log(request.body.image_url);
-  let SQL = 'INSERT INTO works(artist, title, image_url, gallery, century) VALUES ($1, $2, $3, $4, $5) RETURNING id;';
-  let values = [artist, title, image_url, gallery, century];
-  // console.log(values);
-  return client
-    .query(SQL, values)
-    .then(result => {
-      response.redirect(`/works/${result.rows[0].id}`);
-    })
-    .catch((error, response) => handleError(error, response));
+
+  let SQLworks = 'INSERT INTO works(artist, title, image_url, century, gallery_id) VALUES ($1, $2, $3, $4, $5) RETURNING id;';
+  let valuesWorks = [artist, title, image_url, century];
+
+  const SQLgallery = 'SELECT id FROM gallery WHERE name=$1';
+  const valuesGallery = [request.body.gallery];
+
+  client.query(SQLgallery, valuesGallery).then(galleryId => {
+    valuesWorks.push(galleryId.rows[0].id);
+    client.query(SQLworks, valuesWorks)
+      .then(result => response.redirect(`/works/${result.rows[0].id}`))
+      .catch((error, response) => handleError(error, response));
+  });
+}
+
+function createGallery(request, response) {
+  const values = [request.body.gallery];
+  const SQL = `INSERT INTO gallery(name) VALUES ($1)`;
+  client.query(SQL, values).then(() => response.redirect('/'));
 }
 
 function handleError(error, response) {
@@ -201,7 +230,7 @@ function handleError(error, response) {
 //********************************************************************* */ 
 
 function getGalleries() {
-  const SQL = `SELECT DISTINCT gallery FROM works ORDER BY gallery`;
+  const SQL = `SELECT name FROM gallery ORDER BY name`;
   return client.query(SQL);
 }
 
