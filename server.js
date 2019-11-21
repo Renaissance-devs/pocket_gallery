5948a4b3f324464f34f13bb3d7726d6be7c71d7e
David Vloedman <djvloedman@gmail.com>
2019-11-21 10:04:16 -0800

    Update SQL in Update

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ function gallerySelect(request, response) {
     response.redirect('/');
   }
 
-  let SQL = `SELECT * FROM works JOIN gallery ON works.gallery_id=gallery.id WHERE name=$1`;
+  let SQL = `SELECT works.artist, works.title, works.image_url, works.id, gallery.name FROM works JOIN gallery ON works.gallery_id=gallery.id WHERE name=$1`;
   const values = [request.body.gallery];
 
   return client.query(SQL, values)
@@ -243,7 +243,9 @@ function createGallery(request, response) {
 
 function manageGalleries(request, response) {
   getGalleries().then(galleries => {
-    response.render('pages/galleries', {galleries: galleries.rows});
+    response.render('pages/galleries', {
+      galleries: galleries.rows
+    });
   })
 }
 
